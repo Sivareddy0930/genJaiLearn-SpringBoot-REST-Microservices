59b10f1f92b81caeb7fc91aa44f7811d8e0a72af
Sivareddy0930 <sivareddy0930@gmail.com>
2024-08-27 10:16:05 +0530

    Sb-restapis

---
+++ src/main/java/com/Allpackages/StudentsRepo/StudentsRepository.java
@@ -0,0 +1,11 @@
+package com.Allpackages.StudentsRepo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.Allpackages.entity.Students;
+
+@Repository
+public interface StudentsRepository extends JpaRepository<Students, Integer> {
+
+}
